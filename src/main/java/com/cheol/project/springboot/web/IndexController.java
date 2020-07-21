58babdaec8b10dfdec219413b5040909a8884bb3
cheolmin <a7557z@gmail.com>
2020-07-21 18:32:31 +0900

    스프링시큐리티 Session Annotation기반으로 변경

--- src/main/java/com/cheol/project/springboot/web/IndexController.java
+++ src/main/java/com/cheol/project/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.cheol.project.springboot.web;
 
+import com.cheol.project.springboot.config.auth.LoginUser;
+import com.cheol.project.springboot.config.auth.dto.SessionUser;
 import com.cheol.project.springboot.service.posts.PostsService;
 import com.cheol.project.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -32,4 +41,5 @@ public class IndexController {
 
         return "posts-update";
     }
+
 }
