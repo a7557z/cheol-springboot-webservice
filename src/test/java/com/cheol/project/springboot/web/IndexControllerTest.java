0d255cd574108c8835fee284a75e37b8b439b517
cheolmin <a7557z@gmail.com>
2020-08-01 15:43:15 +0900

    layout 주석 제거

--- src/test/java/com/cheol/project/springboot/web/IndexControllerTest.java
+++ src/test/java/com/cheol/project/springboot/web/IndexControllerTest.java
@@ -19,7 +19,7 @@ public class IndexControllerTest {
     public void 메인페이지_로딩(){
         String body = this.restTemplate.getForObject("/", String.class);
 
-        Assertions.assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        Assertions.assertThat(body).contains("게시판");
     }
 
 }
